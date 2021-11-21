27e76328e3cd15514f01ca091075397aa834d0d3
Mario <mariocahyo569@gmail.com>
2021-11-21 09:17:42 +0900

    menambahkan Profil Pada File Calendar.php

--- production/mahasiswa/calendar.php
+++ production/mahasiswa/calendar.php
@@ -55,7 +55,13 @@ if ( !isset($_SESSION["login"])) {
 
             <!-- menu profile quick info -->
             <div class="profile clearfix">
-              
+              <div class="profile_pic">
+                <img src="images/user.png" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2><?php echo $_SESSION['username'] ?></h2>
+              </div>
             </div>
             <!-- /menu profile quick info -->
 
